e7a8d40e50f74d660f4aa47f64e0537ed4418b29
Bartosz Dyja <bartosz.dyja@gmail.com>
2015-07-15 15:05:24 +0200

    FB token authorization

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,11 @@ class NewsController < ApplicationController
   respond_to :json	
   def index
   	token = request.headers['Authorization'].to_s.split(' ').last
-  	respond_with token
+  	user = User.find_by(oauth_token: token)
+  	if user
+  		respond_with user.name
+  	else
+  		respond_with('Unauthorized', status: 	401)
+  	end
   end
 end
